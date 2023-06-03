f46a984e798c278598ef0ad4c5eb2b309f6b3831
Precell <precell@uncommon.org>
2023-06-03 12:46:09 +0200

    textinputs

--- App.js
+++ App.js
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import {TextInput, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
 
   const [name, setName] = useState('Precell')
-  const [person, setPerson] = useState({name:'Joshi', age:29})
-  const clickHandler = () =>{
-    setName('Sehlasie')
-    setPerson({name:"Mario", age:43})
-  }
-
+  const [age, setAge] = useState(53)
   return (
     <View style={styles.container}>
-      <Text>My name is {name}</Text>
-      <Text>His name is {person.name} and his age is {person.age}</Text>
-      <View style={styles.buttonContainer}>
-        <Button title='update state' onPress={clickHandler}/>
+      <Text>Enter Name</Text>
+      <TextInput 
+        multiline
+        style={styles.input}
+        placeholder='e.g. John Doe'
+        onChangeText={(val) =>{setName(val)}}/>
+
+      <Text>Enter age</Text>
+
+      <TextInput
+        keyboardType='numeric'
+        style={styles.input}
+        placeholder='e.g 40'
+        onChangeText={(val) => setAge(val)} />
+        <Text>name :{name}, age: {age}</Text>
       </View>
-    </View>
   );
 }
 
@@ -28,7 +33,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer:{
-    marginTop:20
+  input:{
+    borderWidth: 1,
+    borderColor:"#777",
+    padding:8,
+    margin:10,
+    width:200
   }
-});
+  }
+);
