e8e0c2caf4595dc4e34aa2ef44440e9d36667835
Precell <precell@uncommon.org>
2023-06-03 12:00:07 +0200

    useState and button

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+
+  const [name, setName] = useState('Precell')
+  const [person, setPerson] = useState({name:'Joshi', age:29})
+  const clickHandler = () =>{
+    setName('Sehlasie')
+    setPerson({name:"Mario", age:43})
+  }
+
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.boldText}>Hello, World</Text>
-      </View>
-      <View style={styles.body}>
-        <Text>Loro ipsum dolo sit amet.</Text>
-        <Text>Loro ipsum dolo sit amet.</Text>
-        <Text>Loro ipsum dolo sit amet.</Text>
+      <Text>My name is {name}</Text>
+      <Text>His name is {person.name} and his age is {person.age}</Text>
+      <View style={styles.buttonContainer}>
+        <Button title='update state' onPress={clickHandler}/>
       </View>
     </View>
   );
@@ -23,16 +28,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  header: {
-    backgroundColor:'pink',
-    padding:20
-  },
-  boldText: {
-    fontWeight: 'bold'
-  },
-  body: {
-    backgroundColor:'yellow',
-    padding:20
+  buttonContainer:{
+    marginTop:20
   }
-
 });
